9cdb06edb6d53760e711205b4618b938b7a2a57f
gituser-sudo <48635872+gituser-sudo@users.noreply.github.com>
2026-03-27 20:09:09 +0000

    Fri, Mar 27, 2026, 3:09 PM -05:00

--- minesweeper.py
+++ minesweeper.py
@@ -107,7 +107,7 @@ class Sentence():
         """
         known_mines_cells = set()
         for i, j in self.cells:
-            if (i, j) in super.mines_found:
+            if (i, j) in self.mines_found:
                 known_mines_cells.add((i, j))
 
         return known_mines_cells
@@ -119,7 +119,7 @@ class Sentence():
         """
         known_safe_cells = set()
         for i, j in self.cells:
-            if (i, j) in super.known_safes:
+            if (i, j) in self.known_safes:
                 known_safe_cells.add((i, j))
 
         return known_safe_cells
