371ba80e382e89736059063914dc6805b26e4c75
gituser-sudo <48635872+gituser-sudo@users.noreply.github.com>
2026-04-01 02:31:48 +0000

    Tue, Mar 31, 2026, 9:31 PM -05:00

--- minesweeper.py
+++ minesweeper.py
@@ -132,8 +132,9 @@ class Sentence():
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        self.cells.remove(cell)
-        self.count = self.count - 1
+        if cell in self.cells
+            self.cells.remove(cell)
+            self.count = self.count - 1
 
         self.known_mines.add(cell)
 
